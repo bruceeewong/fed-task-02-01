a001bfa707e8da0a3b52bfc9727e890e1d831d38
bruski <292803281@qq.com>
2020-10-25 22:59:14 +0800

    feat(yeoman): 添加yeoman vue工程脚手架

--- playground/generator-sample/app/index.js
+++ playground/generator-sample/app/index.js
@@ -1,10 +1,24 @@
 const Generator = require("yeoman-generator");
 
 class SampleGenerator extends Generator {
+  prompting() {
+    return this.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Your project name",
+        default: this.appname,
+      },
+    ]).then((answers) => {
+      console.log(answers);
+      this.answers = answers;
+    });
+  }
+
   writing() {
-    const tmpl = this.templatePath("foo.txt");
-    const output = this.destinationPath("foo.txt");
-    const context = { title: "Hello", success: false };
+    const tmpl = this.templatePath("bar.html");
+    const output = this.destinationPath("bar.html");
+    const context = this.answers;
 
     this.fs.copyTpl(tmpl, output, context);
   }
